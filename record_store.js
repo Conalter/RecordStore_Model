eca4953f72bf67bd4f883eca8fd76b4ddea6b17f
Lyle Mitchell <ljwmitchell@gmail.com>
2018-06-08 16:49:39 +0100

    adds record_store & stock

---
+++ record_store.js
@@ -0,0 +1,6 @@
+var Record_Store = function(){
+  this.name = Goldrush Records;
+  this.city = Perth;
+  this.balance = balance;
+  this.stock = Stock;
+};
---
+++ stock.js
@@ -0,0 +1,8 @@
+var Stock = function (artist, title, genre, price){
+  this.artist = artist;
+  this.title = title;
+  this.genre = genre;
+  this.price = price;
+};
+
+model.exports = Stock; 
